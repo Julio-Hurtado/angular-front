f55ccfa75cc34ad44d1df8545e24de7218e68044
Ernesto Hurtado <calerojulio92@gmail.com>
2022-11-21 17:26:13 -0600

    import and export paginator component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { PaginatorComponent } from './paginator/paginator.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, PaginatorComponent],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, PaginatorComponent],
 })
 export class SharedModule {}
