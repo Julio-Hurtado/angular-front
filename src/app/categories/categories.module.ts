96a2484436641f5a6fdf65aaee3ebc7877960477
Ernesto Hurtado <calerojulio92@gmail.com>
2022-11-21 17:27:34 -0600

    import shared module

--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { CategoryComponent } from './components/category/category.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CategoriesComponent, CategoryComponent],
-  imports: [CommonModule, CategoriesRoutingModule],
+  imports: [CommonModule, CategoriesRoutingModule, SharedModule],
 })
 export class CategoriesModule {}
