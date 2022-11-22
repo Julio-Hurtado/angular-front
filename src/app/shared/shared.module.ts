dfa0a1263c98dc4fb98cd2feacb1d2ac95e4bf60
Ernesto Hurtado <calerojulio92@gmail.com>
2022-11-21 18:54:13 -0600

    import and export spinner component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { PaginatorComponent } from './paginator/paginator.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
-  declarations: [HeaderComponent, PaginatorComponent],
+  declarations: [HeaderComponent, PaginatorComponent, SpinnerComponent],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, PaginatorComponent],
+  exports: [HeaderComponent, PaginatorComponent, SpinnerComponent],
 })
 export class SharedModule {}
