981f4e6e673a3e9a968abf15315aad710241bb85
Ernesto Hurtado <calerojulio92@gmail.com>
2022-11-26 18:29:44 -0600

    formgroup, validations and create method added,

--- src/app/categories/components/category/category.component.ts
+++ src/app/categories/components/category/category.component.ts
@@ -1,15 +1,88 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+  min = 5;
+  max = 50;
+  categoryForm!: FormGroup;
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly categorySvc: CategoryService,
+    private readonly router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.categoryForm = this.buildForm();
   }
 
+  private buildForm() {
+    return this.fb.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(this.min),
+          Validators.maxLength(this.max),
+        ],
+      ],
+      image: ['', [Validators.required]],
+    });
+  }
+
+  public get nameField() {
+    return this.categoryForm.get('name');
+  }
+
+  public get imageField() {
+    return this.categoryForm.get('image');
+  }
+
+  public get isNameFieldValid(): boolean {
+    return this.nameField!.touched && this.nameField!.valid;
+  }
+
+  public get isNameFieldInvalid(): boolean {
+    return this.nameField!.touched && this.nameField!.invalid;
+  }
+
+  public get isImageFieldValid(): boolean {
+    return this.imageField!.touched && this.imageField!.valid;
+  }
+
+  public get isImageFieldInvalid(): boolean {
+    return this.imageField!.touched && this.imageField!.invalid;
+  }
+
+  uploadFile(event: Event): void {
+    const file = (<HTMLInputElement>event.target).files!.item(0);
+    if (file) {
+      this.imageField?.patchValue(file);
+    }
+  }
+
+  sendDataToApi(e: Event): void {
+    if (this.categoryForm.valid) {
+      e.preventDefault();
+      this.createCategory();
+    }
+    this.categoryForm.markAllAsTouched();
+  }
+
+  private createCategory(): void {
+    const formData = new FormData();
+    formData.append('name', this.nameField?.value);
+    formData.append('file', this.imageField?.value);
+
+    this.categorySvc.saveCategory(formData).subscribe((res) => {
+      console.log(res.mesage);
+      this.router.navigateByUrl('/categories');
+    });
+  }
 }
