57ce37869f4ad6e50358e49baa6d2e9c28f3ac78
Ernesto Hurtado <calerojulio92@gmail.com>
2023-01-31 01:00:44 -0600

    add route for edit category

--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { CategoryEditComponent } from './components/category-edit/category-edit.component';
 import { CategoryComponent } from './components/category/category.component';
 
 const routes: Routes = [
   { path: 'list', component: CategoriesComponent },
   { path: 'create', component: CategoryComponent },
+  { path: 'edit/:id', component: CategoryEditComponent },
   { path: '**', redirectTo: 'list' },
 ];
 
